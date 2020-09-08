2c45c2e09b33f9f06163b2f3104d50bf99fb3ed8
dwq0048 <dwq0048@gmail.com>
2020-09-09 00:51:41 +0900

    게시판 뷰 수정

--- controllers/api/board/view.controller.js
+++ controllers/api/board/view.controller.js
@@ -1,22 +1,50 @@
 const Schema = require('../../../models/functions');
 
 const view = (req, res, next) => {
+    // Response Result
+    const onResponse = (payload) => {
+        res.status(200).json({
+            state : 'success',
+            payload
+        });
+    }
+
+    const onError = (message) => {
+        res.status(401).json({
+            state : 'error',
+            message : message
+        });
+    }
+    // Response Result End
+
     const data = {
         index: req.body.index,
         board: req.body.board
     }
 
-    Schema.POST.View(data).then((req) => {
-        res.status(200).json({
-            state: 'success',
-            payload : req[0]
-        })    
-    }).catch((e) => {
-        res.status(401).json({
-            state: 'error',
-            e
+    let ViewRequest = {};
+
+    const ViewPost = async () => {
+        await Schema.POST.View(data).then((req) => {
+            ViewRequest = { status : true, payload : req }
+        }).catch((error) => {
+            throw new Error(error.message);
         })
-    })
+
+        return ViewRequest;
+    }
+
+    const RunCommand = async () => {
+        try{
+            const ResultView = await ViewPost();
+            onResponse(ResultView.payload[0]);
+        } catch (error){
+            console.log(error);
+            onError(error.message);
+        }
+    }
+    RunCommand();
+
 }
 
 module.exports = view;
