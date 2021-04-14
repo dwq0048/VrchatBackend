1d12fbba0e94add06e7f7a37a2bdd230007b82b0
dwq0048 <dwq0048@gmail.com>
2021-04-14 21:00:34 +0900

    라우터 추가 / token invalid signarture 오류추가 해야됨

---
+++ routes/api/v1/board.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const Middleware = {
+	TOKEN : require('../../../controllers/middleware/token.middleware'),
+	VIEWS : require('../../../controllers/middleware/views.middleware'),
+	FORMS : require('../../../controllers/middleware/forms.middleware'),
+}
+
+// Read
+router.post('/read/list/:id', require('../../../controllers/api/board/read/list.controller'));
+router.post('/read/view/:id', Middleware.TOKEN, Middleware.VIEWS, require('../../../controllers/api/board/read/view.controller'));
+router.post('/read/post/like', Middleware.TOKEN, require('../../../controllers/api/board/read/post.like.controller'));
+router.post('/read/comment', require('../../../controllers/api/board/read/comment.controller'));
+
+// Write
+router.post('/write/post', Middleware.TOKEN, Middleware.FORMS({ path : 'post', name : 'images', file : { type : 'array' } }), require('../../../controllers/api/board/write/post.controller'));
+router.post('/write/post/like', Middleware.TOKEN, require('../../../controllers/api/board/write/post.like.controller'))
+router.post('/write/comment', Middleware.TOKEN, require('../../../controllers/api/board/write/comment.controller'));
+
+module.exports = router;
